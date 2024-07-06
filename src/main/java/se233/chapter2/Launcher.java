57d747df8529aec442eb3d7f49c29a52bcb0203f
MT <minthantko101@gmail.com>
2024-07-06 23:25:25 +0700

    extractFromApi

--- src/main/java/se233/chapter2/Launcher.java
+++ src/main/java/se233/chapter2/Launcher.java
@@ -1,4 +1,17 @@
 package se233.chapter2;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import se233.chapter2.controller.FetchData;
+
+public class Launcher extends Application {
+    public static Stage primaryStage;
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage=stage;
+        primaryStage.setTitle("Currency Watcher");
+        primaryStage.setResizable(false);
+        System.out.println(FetchData.fetchRange("USD", 6));
+        primaryStage.show();
+    }
 }

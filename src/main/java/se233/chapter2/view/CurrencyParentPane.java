7277f5c75e5b8402f79a71ddeb30b9e24082b7ea
MT <minthantko101@gmail.com>
2024-07-14 18:03:16 +0700

    changedToScrollable

--- src/main/java/se233/chapter2/view/CurrencyParentPane.java
+++ src/main/java/se233/chapter2/view/CurrencyParentPane.java
@@ -1,22 +1,31 @@
 package se233.chapter2.view;
 
 import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import se233.chapter2.model.Currency;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class CurrencyParentPane extends FlowPane {
+public class CurrencyParentPane extends ScrollPane {
+    VBox vBox = new VBox();
+
     public CurrencyParentPane(List<Currency> currencyList) throws ExecutionException, InterruptedException {
         this.setPadding(new Insets(0));
         refreshPane(currencyList);
+        this.setFitToWidth(true);
+        this.setHbarPolicy(ScrollBarPolicy.NEVER);
+        this.setMaxHeight(450);
+        this.setContent(vBox);
     }
     public void refreshPane(List<Currency> currencyList) throws ExecutionException, InterruptedException {
-        this.getChildren().clear();
+        vBox.getChildren().clear();
         for(int i=0; i<currencyList.size(); i++) {
             CurrencyPane cp = new CurrencyPane(currencyList.get(i));
-            this.getChildren().add(cp);
+            vBox.getChildren().add(cp);
         }
     }
+
 }
